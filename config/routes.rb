cd786ed4448dd2d20015d72651135f7bef07bc88
keoshayoung <keoshayoung@email.com>
2022-10-04 14:44:33 -0400

    added Friends Update route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "/friends" => "friends#index"
   post "/friends" => "friends#create"
   get "/friends/:id" => "friends#show"
+  patch "/friends/:id" => "friends#update"
 end
