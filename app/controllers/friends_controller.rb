be302785ec5f692341655f083d013007e2e216ee
keoshayoung <keoshayoung@email.com>
2022-10-04 14:11:13 -0400

    created create action

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,4 +3,19 @@ class FriendsController < ApplicationController
     friends = Friend.all.order(:id)
     render json: friends
   end
+
+  def create
+    friend = Friend.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone: params[:phone],
+      twitter: params[:twitter],
+    )
+    if friend.save
+      render json: friend
+    else
+      render json: { errors: friend.errors.full_messages }, status: :bad_request
+    end
+  end
 end
