44bce9dacb26be75864231296782d8404ebb4818
keoshayoung <keoshayoung@email.com>
2022-10-04 14:43:25 -0400

    created update action

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -24,4 +24,18 @@ class FriendsController < ApplicationController
     friend = Friend.find_by(id: params[:id])
     render json: friend
   end
+
+  def update
+    friend = Friend.find_by(id: params[:id])
+    friend.first_name = params[:first_name] || friend.first_name
+    friend.last_name = params[:last_name] || friend.last_name
+    friend.email = params[:email] || friend.email
+    friend.phone = params[:phone] || friend.phone
+    friend.twitter = params[:twitter] || friend.twitter
+    if friend.save
+      render json: friend
+    else
+      render json: { errors: friend.errors.full_messages }, status: :bad_request
+    end
+  end
 end
